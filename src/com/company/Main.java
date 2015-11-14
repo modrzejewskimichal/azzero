da97eb9b3f808f59e9bf2678bbbc22c1bfb96af3
tulipan <modrzejewskimichal@wp.pl>
2015-11-14 12:24:34 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Podaj liczb�");
+        Scanner wej = new Scanner(System.in);
+        int c, b=0;
+        for(int a=0; b!=0; b=wej.nextInt()){
+            c=a+b;
+
+
+
+            System.out.println(c);
+        }
+
+        // write your code here
+    }
+}
