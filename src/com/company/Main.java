32b28f101e6b5303af679505fced89b6d8645a50
tulipan <modrzejewskimichal@wp.pl>
2015-11-15 00:22:57 +0100

    kommit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Podaj liczb�");
+        System.out.println("Podaj liczbe");
         Scanner wej = new Scanner(System.in);
-        int c, b=0;
-        for(int a=0; b!=0; b=wej.nextInt()){
-            c=a+b;
+        int a = wej.nextInt();
+        while (a != 0) {
+            if (a == 0) {
+                System.out.println(a);
+            }
+            System.out.println("Podaj liczbe");
+            Scanner b = new Scanner(System.in);
+            int d = b.nextInt();
+            System.out.println(d);
 
-
-
-            System.out.println(c);
         }
 
         // write your code here
